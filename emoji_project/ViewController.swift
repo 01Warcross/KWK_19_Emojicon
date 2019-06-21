94b680489b86ddb93a2f79bfb8dd4b8c6cf1be8b
Claudia Pérez <iphone.claudia@icloud.com>
2019-06-21 09:58:22 -0500

    Complete Itereation 1

--- emoji_project/ViewController.swift
+++ emoji_project/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "AYEE!!", message: "yah think you got lucky didn't yah? ha!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+// @IBAction func showMessage1(sender: UIButton) {
+//    let alertController = UIAlertController(title: "AYEE!!", message: "Yah Greedy Child! Of course you sprung for the money -_-", preferredStyle:UIAlertController.Style.alert)
+//    
+//    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.style.default,handler: nil))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
